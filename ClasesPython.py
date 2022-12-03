a43ea05e9a86b8425171e1a0d86f1de1eee1f61c
Luis Andres Sepulveda <110847118+Anothher@users.noreply.github.com>
2022-12-03 12:41:48 -0500

    Cambios

--- ClasesPython.py
+++ ClasesPython.py
@@ -10,33 +10,6 @@ y=1/operacion4
 print("Resultado es",y)
 
 
-#variables
-
-
-print("Para el genero Niño...")
-xnBraga=int(input("Ingrese la cantidad soliciada del articulo Braga: "))
-xnJean=int(input("Ingrese la cantidad soliciada del articulo Jean: "))
-xnPantalon=int(input("Ingrese la cantidad soliciada del articulo Pantalon: "))
-
-print("Para el genero Mujer...")
-xmBraga=int(input("Ingrese la cantidad soliciada del articulo Braga: "))
-xmJean=int(input("Ingrese la cantidad soliciada del articulo Jean: "))
-xmPantalon=int(input("Ingrese la cantidad soliciada del articulo Pantalon: "))
-
-print("Para el genero Hombre...")
-xhBraga=int(input("Ingrese la cantidad soliciada del articulo Braga: "))
-xhJean=int(input("Ingrese la cantidad soliciada del articulo Jean: "))
-xhPantalon=int(input("Ingrese la cantidad soliciada del articulo Pantalon: "))
-print("\n")
-print("Total de cantidades pedidas del genero niño es:", xnBraga + xnJean + xnPantalon)
-print("Total de cantidades pedidas del genero Mujer es:", xmBraga + xmJean + xmPantalon)
-print("Total de cantidades pedidas del genero Hombre es:", xhBraga + xhJean + xhPantalon, "\n" )
-
-print("Total de cantidades pedidas del Articulo Braga fue de:", xnBraga + xmBraga + xhBraga)
-print("Total de cantidades pedidas del Articulo Jeans fue de:", xnJean + xmJean + xhJean)
-print("Total de cantidades pedidas del Articulo Pantalon fue de:", xnPantalon + xmPantalon + xhPantalon)
-
-
 #bucles
 
 
